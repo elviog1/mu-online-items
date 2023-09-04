d32900bc82bb96aa6fed99335f29e67aeda5342b
Elvio <elvio.galeano@gmail.com>
2023-09-04 13:19:28 -0300

    se agregaron propiedades nuevas

--- src/items/interfaces/items.interface.ts
+++ src/items/interfaces/items.interface.ts
@@ -1,11 +1,16 @@
 export interface Item {
     name: string;
+    set:string;
     level: number;
     str: number;
     agi:number;
     type:string;
-    image:string;
+    image: { url: string; level: number }[];
+    defense:number;
+    durability:number;
+    attackSpeed:number;
+    class:string;
     upgradeStr:number;
     upgradeAgi:number;
-    set:string;
+    upgradeDefense:number;
 }
