9880c5624a9902fce484a5599a243216be949f5a
Elvio <elvio.galeano@gmail.com>
2023-09-12 00:59:21 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors();
-  await app.listen(4016);
+  await app.listen(4018);
 }
 bootstrap();
